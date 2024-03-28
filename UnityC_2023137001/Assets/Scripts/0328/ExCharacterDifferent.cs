7aeba3a4233c92cb54557b4b8f4705017471e85d
Hodawn <happygy925@naver.com>
2024-03-28 10:40:11 +0900

    Feat: 0328

---
+++ UnityC_2023137001/Assets/Scripts/0328/ExCharacterDifferent.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExCharacterDifferent : MonoBehaviour
+{
+    //protected override void Move()
+    //{
+        //base.Move();
+        //transform.Translate(
+            //Vector3.up * speed * Time.deltaTime);
+    //}
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
